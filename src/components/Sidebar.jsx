f6bbcfcf29b198ec5e5e1029a791e9b507db9562
trevharri <trevor.harri94@gmail.com>
2023-06-09 12:47:39 +0200

    changed logo on mobile

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -37,7 +37,7 @@ const Sidebar = () => {
         ) : <HiOutlineMenu className='w-6 h-6 text-white mr-2' onClick={() => setMobileMenuOpen(true)}/>}
       </div>
       <div className={`absolute top-0 h-screen w-2/3 bg-gradient-to-tl from white/10 to-[#483d8b] backdrop-blur-lg z-10 p-6 md:hidden smooth-transition ${mobileMenuOpen ? 'left-0' : '-left-full' }`}>
-        <img src={logo} alt="logo" className="w-full h-14 object-contain" />
+        <img src={newMusicLogo} alt="logo" className="w-full h-20 object-contain" />
         <NavLinks handleClick={() => setMobileMenuOpen(false)} />
       </div>
     </>
